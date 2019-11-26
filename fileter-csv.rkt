63e4d2d408a37d35c5b29b9c0d8ef8650a870d25
Paul Lu <luhanfeng1106@sina.com>
2019-11-25 19:00:40 -0600

    finish read from cmd line

--- fileter-csv.rkt
+++ fileter-csv.rkt
@@ -1,9 +1,17 @@
 #lang racket
+ (require racket/cmdline)
+(define path (vector-ref (current-command-line-arguments) 0))
 (require csv-reading)
 (define make-food-csv-reader
   (make-csv-reader-maker
    '(
      (strip-leading-whitespace?  . #t)
      (strip-trailing-whitespace? . #t))))
-(define next-row (make-food-csv-reader (open-input-file "./RAW_interactions.csv")))
+(define next-row (make-food-csv-reader (open-input-file path)))
+(define csv-writer
+  (lambda (out inp)
+    (display out)
+    ))
+(display (next-row))
+(display (next-row))
 (display (next-row))
